b258549dd3e8a8882b46ffe31fa553fad47a2267
Sergio <smccr123@gmail.com>
2021-09-12 15:36:44 -0600

    Exercise 10.6: the sign in view

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from './theme';
 
@@ -7,6 +8,8 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.textPrimary,
+    display: 'flex',
+    flexDirection: 'row'
   },
   text: {
     color: 'white',
@@ -19,7 +22,14 @@ const AppBar = () => {
   return (
     <View style={styles.container}>
       <Pressable>
-        <Text style={styles.text}>Repositories</Text>
+        <Link to="/">
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
+      </Pressable>
+      <Pressable>
+        <Link to="/signin">
+          <Text style={styles.text}>Sign in</Text>
+        </Link>
       </Pressable>
     </View>
   );

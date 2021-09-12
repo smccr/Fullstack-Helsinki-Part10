2dc32f17afe2faa3e311b474066eda289d40f71f
Sergio <smccr123@gmail.com>
2021-09-11 21:47:16 -0600

    Exercise 10.4: the app bar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
+import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
 
 const styles = StyleSheet.create({
@@ -14,6 +15,7 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View style={styles.container}>
+      <AppBar />
       <RepositoryList />
     </View>
   );

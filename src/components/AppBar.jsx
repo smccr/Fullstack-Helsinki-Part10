0b268f3e8c630dae0343970a9268638f5ddc9543
Sergio <smccr123@gmail.com>
2021-09-12 21:45:02 -0600

    Exercise 10.7: scrollable app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from './theme';
@@ -21,16 +21,18 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
-        <Link to="/">
-          <Text style={styles.text}>Repositories</Text>
-        </Link>
-      </Pressable>
-      <Pressable>
-        <Link to="/signin">
-          <Text style={styles.text}>Sign in</Text>
-        </Link>
-      </Pressable>
+      <ScrollView horizontal>
+        <Pressable>
+          <Link to="/">
+            <Text style={styles.text}>Repositories</Text>
+          </Link>
+        </Pressable>
+        <Pressable>
+          <Link to="/signin">
+            <Text style={styles.text}>Sign in</Text>
+          </Link>
+        </Pressable>
+      </ScrollView>
     </View>
   );
 };

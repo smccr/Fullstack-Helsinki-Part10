583f2f3efe3a11b20d2ab85f82f612f1db217a11
Sergio <smccr123@gmail.com>
2021-09-14 22:54:28 -0600

    Exercise 10.9: validating the sign in form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Pressable, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 import FormikTextInput from './FormikTextInput';
 import theme from './theme';
 import Text from './Text';
@@ -31,12 +32,26 @@ const styles = StyleSheet.create({
   }
 });
 
+const validationSchema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required')
+});
+
 
 const SignInForm = ({ onSubmit }) => {
   return (
     <View style={styles.container}>
-      <FormikTextInput style={styles.formField} name="username" placeholder="Username" />
-      <FormikTextInput style={styles.formField} secureTextEntry name="password" placeholder="Password" />
+      <FormikTextInput
+        style={styles.formField}
+        name="username"
+        placeholder="Username"
+      />
+      <FormikTextInput
+        style={styles.formField}
+        secureTextEntry
+        name="password"
+        placeholder="Password"
+      />
       <Pressable onPress={onSubmit}>
         <Text style={styles.button}>Sign in</Text>
       </Pressable>
@@ -50,7 +65,12 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+import theme from './theme';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  errorBorder: {
+    borderColor: theme.colors.errorColor
+  }
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = [style, error&& styles.errorBorder];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
